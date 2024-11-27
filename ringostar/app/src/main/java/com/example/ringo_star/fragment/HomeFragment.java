977db262198664598a43373b04c806618dbce828
cosimocurri <cosimo.curri@student.put.poznan.pl>
2024-11-27 13:53:42 +0100

    .

--- ringostar/app/src/main/java/com/example/ringo_star/fragment/HomeFragment.java
+++ ringostar/app/src/main/java/com/example/ringo_star/fragment/HomeFragment.java
@@ -8,13 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import com.example.ringo_star.MainActivity;
 import com.example.ringo_star.R;
-import com.example.ringo_star.UserInfoActivity;
 import com.example.ringo_star.components.QuestionnaireBoxComponent;
 import com.example.ringo_star.questionnaire.HowAreYouQuestionnaire;
+import com.example.ringo_star.questionnaire.TreatmentQuestionnaire;
 
 public class HomeFragment extends Fragment {
     public HomeFragment() {}
@@ -29,11 +27,19 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         QuestionnaireBoxComponent questionnaireGeneral = view.findViewById(R.id.questionnaireGeneral);
+        QuestionnaireBoxComponent questionnaireTreatment = view.findViewById(R.id.questionnaireTreatment);
 
         questionnaireGeneral.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 handleButtonClick(HowAreYouQuestionnaire.class);
+            }
+        });
+
+        questionnaireTreatment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleButtonClick(TreatmentQuestionnaire.class);
             }
         });
 
